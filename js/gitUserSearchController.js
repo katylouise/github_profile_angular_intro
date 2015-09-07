c8b4a99971dc5ce9a05bebe30a983781ded5d796
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-07 18:08:01 +0100

    Removed access token

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,12 +1,10 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
   var self = this;
   var searchResource = $resource('https://api.github.com/search/users');
-  var token = 'c7a27311b3ece356b23fa903409c47f42a5e24ef';
 
   self.doSearch = function() {
     self.searchResult = searchResource.get(
-      { q: self.searchTerm,
-        access_token: self.token }
+      { q: self.searchTerm }
     );
   };
 }]);
